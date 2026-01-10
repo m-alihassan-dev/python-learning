a7b910684ecf6fe317a1367c6706cd03ad22b1ff
Ali Hassan <m.alihassan.dev@gmail.com>
2026-01-10 14:38:13 +0500

    Explicit Type Casting Example

---
+++ explicit_typecasting.py
@@ -0,0 +1,19 @@
+a = 5
+b = str(a)
+print(type(a), b)
+
+c = "5"
+d = int(c)
+print(type(c), d)
+
+e = 1
+f = float(e)
+print(type(e), f)
+
+g = 1
+h = bool(g)
+print(type(g), h)
+
+i = 0
+j = bool(i)
+print(type(i), j)
