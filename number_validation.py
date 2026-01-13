9df7789461201237f55f672e294161b97614c453
Ali Hassan <m.alihassan.dev@gmail.com>
2026-01-14 03:47:05 +0500

    Logical Condition Validation Example

--- number_validation.py
+++ number_validation.py
@@ -1,6 +1,6 @@
-n = int(input("Enter a number: "))
+num = int(input("Enter a number: "))
 
-if n > 0 and (n % 2 == 0 or n % 5 == 0):
+if num > 0 and (num % 2 == 0 or num % 5 == 0):
     print("Valid Number")
 else:
     print("Invalid Number")
